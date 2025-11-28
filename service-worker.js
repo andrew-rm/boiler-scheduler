a4af43ca576dd85ff59290f4d7e374a4159caa68
andrew-rm <jliker2024@gmail.com>
2025-11-28 23:48:21 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = "boiler-scheduler-v1";
+// 배포할 때마다 버전 숫자 꼭 올리기!
+const CACHE_NAME = "boiler-scheduler-v3";
+
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.webmanifest"
-  // 필요하면 아이콘 등 파일도 추가
+  "./manifest.webmanifest",
   // "./icon-192.png",
   // "./icon-512.png"
 ];
 
+// 새 서비스워커 설치되면 바로 활성화
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
+// 기존 탭도 즉시 새로운 서비스워커 사용
 self.addEventListener("activate", event => {
+  clients.claim();
   event.waitUntil(
     caches.keys().then(cacheNames =>
       Promise.all(
@@ -28,11 +31,18 @@ self.addEventListener("activate", event => {
   );
 });
 
+// 최신 파일 우선 가져오되, 실패하면 캐시 사용
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // 캐시에 있으면 캐시 사용, 없으면 네트워크
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        // 받은 최신 데이터를 캐시에 저장
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request)) // 오프라인이면 캐시 fallback
   );
 });
